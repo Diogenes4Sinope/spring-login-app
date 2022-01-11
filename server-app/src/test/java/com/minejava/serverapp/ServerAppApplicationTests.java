5034169643d7768933e717ee82458b8b0a14d21d
Diogenes4Sinope <talk2nategwu@protonmail.com>
2022-01-11 10:46:25 +0000

    Boostrapped spring boot

---
+++ server-app/src/test/java/com/minejava/serverapp/ServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minejava.serverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
